e960b27d71d1b0e0743f531d2132da272042ed75
54785 <547858171@qq.com>
2026-04-06 13:56:15 +0800

    fix:车辆查询

--- ui/tab_valuation.py
+++ ui/tab_valuation.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout,
                               QTableWidget, QTableWidgetItem, QLabel, QPushButton,
                               QComboBox, QLineEdit, QGroupBox, QHeaderView, QAbstractItemView,
-                              QMessageBox, QFrame, QScrollArea, QSpinBox)
+                              QMessageBox, QFrame, QScrollArea, QSpinBox, QSizePolicy)
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont, QPixmap
 import json
@@ -18,9 +18,11 @@ class ValuationTab(QWidget):
         self.init_ui()
 
     def init_ui(self):
-        layout = QHBoxLayout(self)
+        main_layout = QHBoxLayout()
 
-        left_panel = QVBoxLayout()
+        left_panel = QWidget()
+        left_layout = QVBoxLayout()
+        left_layout.setContentsMargins(3, 3, 3, 3)
 
         search_group = QGroupBox("搜索条件")
         search_layout = QVBoxLayout()
@@ -81,7 +83,7 @@ class ValuationTab(QWidget):
         search_layout.addWidget(search_btn)
 
         search_group.setLayout(search_layout)
-        left_panel.addWidget(search_group)
+        left_layout.addWidget(search_group)
 
         self.car_list = QTableWidget()
         self.car_list.setColumnCount(8)
@@ -89,8 +91,8 @@ class ValuationTab(QWidget):
         self.car_list.setSelectionBehavior(QAbstractItemView.SelectRows)
         self.car_list.setSelectionMode(QAbstractItemView.SingleSelection)
         self.car_list.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeToContents)
-        self.car_list.horizontalHeader().setSectionResizeMode(1, QHeaderView.Stretch)  # QHeaderView.Stretch
-        self.car_list.horizontalHeader().setSectionResizeMode(2, QHeaderView.Stretch)  # QHeaderView.Stretch
+        self.car_list.horizontalHeader().setSectionResizeMode(1, QHeaderView.Stretch)
+        self.car_list.horizontalHeader().setSectionResizeMode(2, QHeaderView.Stretch)
         self.car_list.horizontalHeader().setSectionResizeMode(3, QHeaderView.ResizeToContents)
         self.car_list.horizontalHeader().setSectionResizeMode(4, QHeaderView.Stretch)
         self.car_list.horizontalHeader().setSectionResizeMode(5, QHeaderView.Stretch)
@@ -98,25 +100,34 @@ class ValuationTab(QWidget):
         self.car_list.horizontalHeader().setSectionResizeMode(7, QHeaderView.ResizeToContents)
         self.car_list.setEditTriggers(QAbstractItemView.NoEditTriggers)
         self.car_list.cellClicked.connect(self.on_car_selected)
-        left_panel.addWidget(self.car_list)
+        left_layout.addWidget(self.car_list)
 
-        layout.addLayout(left_panel, 1)
+        left_panel.setLayout(left_layout)
 
-        right_panel = QVBoxLayout()
+        right_panel = QWidget()
+        right_layout = QVBoxLayout()
+        right_layout.setContentsMargins(3, 3, 3, 3)
+        right_layout.setSpacing(5)
 
         detail_group = QGroupBox("车辆详情与报价")
+        detail_group.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         detail_layout = QVBoxLayout()
 
-        self.detail_label = QLabel("请选择左侧车辆查看详情")
-        self.detail_label.setAlignment(Qt.AlignCenter)
-        self.detail_label.setFont(QFont("Microsoft YaHei", 10))
-        detail_layout.addWidget(self.detail_label)
-
+        top_layout = QHBoxLayout()
+        top_layout.setSpacing(10)
         self.car_image_label = QLabel()
         self.car_image_label.setAlignment(Qt.AlignCenter)
-        self.car_image_label.setFixedSize(400, 300)
+        self.car_image_label.setFixedSize(350, 280)
         self.car_image_label.setStyleSheet("background-color: #f0f0f0; border: 1px solid #ccc;")
-        detail_layout.addWidget(self.car_image_label)
+        top_layout.addWidget(self.car_image_label)
+
+        self.detail_label = QLabel("请选择左侧车辆查看详情")
+        self.detail_label.setFont(QFont("Microsoft YaHei", 10))
+        self.detail_label.setAlignment(Qt.AlignLeft | Qt.AlignTop)
+        self.detail_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        top_layout.addWidget(self.detail_label)
+
+        detail_layout.addLayout(top_layout)
 
         self.price_info = QGroupBox("参考报价")
         price_layout = QVBoxLayout()
@@ -125,14 +136,19 @@ class ValuationTab(QWidget):
         self.price_table.setHorizontalHeaderLabels(["来源", "品牌", "型号", "年份", "参考价"])
         self.price_table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         self.price_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.price_table.setMaximumHeight(150)
         price_layout.addWidget(self.price_table)
         self.price_info.setLayout(price_layout)
         detail_layout.addWidget(self.price_info)
 
         detail_group.setLayout(detail_layout)
-        right_panel.addWidget(detail_group)
+        right_layout.addWidget(detail_group)
 
-        layout.addLayout(right_panel, 1)
+        right_panel.setLayout(right_layout)
+
+        main_layout.addWidget(left_panel, 0)
+        main_layout.addWidget(right_panel, 0)
+        self.setLayout(main_layout)
 
     def load_brands(self):
         brands = ["丰田", "本田", "日产", "大众", "宝马", "奔驰", "奥迪", "别克", "福特", "雪佛兰",
@@ -207,53 +223,56 @@ class ValuationTab(QWidget):
             QMessageBox.information(self, "提示", "未找到匹配的车辆数据，请先运行爬虫任务。")
 
     def on_car_selected(self, row, col):
+        car_type = self.car_type.currentText()
+        if car_type == "二手车":
+            return
         car = self.car_list.item(row, 0).data(Qt.UserRole)
         if not car:
             return
 
-        car_type = car.get("_type", "")
-        detail_text = f"类型: {car_type}\n"
-        detail_text += f"品牌: {car.get('che_liang_pin_pai', '')}\n"
-        detail_text += f"车系: {car.get('xuan_ze_xi_lie', '')}\n"
-        detail_text += f"车型: {car.get('xuan_ze_zi_xi_lie', '')}\n"
-        detail_text += f"年份: {car.get('chu_chang_ri_qi', '')}\n"
-        detail_text += f"起拍价: ¥{car.get('yi_kou_jia', 0):,.2f}\n"
-        detail_text += f"最新出价: {car.get('zui_xin_chu_jia', '')}\n"
-        detail_text += f"拍卖时间: {car.get('pai_mai_hui_start_time', '')}\n"
-        detail_text += f"是否新能源: {'是' if car.get('is_xin_neng_yuan') == 1 else '否'}\n"
-        detail_text += f"网站: {car.get('site_name', '')}\n"
-        detail_text += f"链接: {car.get('detail_urls', '')}"
-
-        self.detail_label.setText(detail_text)
-        self.detail_label.setAlignment(Qt.AlignLeft | Qt.AlignTop)
-
-        brand = car.get("che_liang_pin_pai", "")
-        model = car.get("xuan_ze_zi_xi_lie", "")
-        year = car.get("chu_chang_ri_qi", 0)
-
-        where_clauses = []
-        params = {}
-        if brand:
-            where_clauses.append("che_liang_pin_pai LIKE :brand")
-            params["brand"] = f"%{brand}%"
-        if model:
-            where_clauses.append("xuan_ze_zi_xi_lie LIKE :model")
-            params["model"] = f"%{model}%"
-        if year:
-            where_clauses.append("chu_chang_ri_qi = :year")
-            params["year"] = year
-
-        where_sql = " AND ".join(where_clauses) if where_clauses else "1=1"
-
-        table = "accident_car" if car_type == "事故车" else "used_car"
-        similar_cars = self.db.query(
-            f"SELECT site_name, che_liang_pin_pai, xuan_ze_zi_xi_lie, chu_chang_ri_qi, yi_kou_jia FROM {table} WHERE {where_sql} LIMIT 20", params
-        )
-
-        self.price_table.setRowCount(len(similar_cars))
-        for i, sc in enumerate(similar_cars):
-            self.price_table.setItem(i, 0, QTableWidgetItem(sc.get("site_name", "")))
-            self.price_table.setItem(i, 1, QTableWidgetItem(sc.get("che_liang_pin_pai", "")))
-            self.price_table.setItem(i, 2, QTableWidgetItem(sc.get("xuan_ze_zi_xi_lie", "")))
-            self.price_table.setItem(i, 3, QTableWidgetItem(str(sc.get("chu_chang_ri_qi", ""))))
-            self.price_table.setItem(i, 4, QTableWidgetItem(f"¥{sc.get('yi_kou_jia', 0):,.2f}"))
+        # car_type = car.get("_type", "")
+        # detail_text = f"类型: {car_type}\n"
+        # detail_text += f"品牌: {car.get('che_liang_pin_pai', '')}\n"
+        # detail_text += f"车系: {car.get('xuan_ze_xi_lie', '')}\n"
+        # detail_text += f"车型: {car.get('xuan_ze_zi_xi_lie', '')}\n"
+        # detail_text += f"年份: {car.get('chu_chang_ri_qi', '')}\n"
+        # detail_text += f"起拍价: ¥{car.get('yi_kou_jia', 0):,.2f}\n"
+        # detail_text += f"最新出价: {car.get('zui_xin_chu_jia', '')}\n"
+        # detail_text += f"拍卖时间: {car.get('pai_mai_hui_start_time', '')}\n"
+        # detail_text += f"是否新能源: {'是' if car.get('is_xin_neng_yuan') == 1 else '否'}\n"
+        # detail_text += f"网站: {car.get('site_name', '')}\n"
+        # detail_text += f"链接: {car.get('detail_urls', '')}"
+        #
+        # self.detail_label.setText(detail_text)
+        # self.detail_label.setAlignment(Qt.AlignLeft | Qt.AlignTop)
+        #
+        # brand = car.get("che_liang_pin_pai", "")
+        # model = car.get("xuan_ze_zi_xi_lie", "")
+        # year = car.get("chu_chang_ri_qi", 0)
+        #
+        # where_clauses = []
+        # params = {}
+        # if brand:
+        #     where_clauses.append("che_liang_pin_pai LIKE :brand")
+        #     params["brand"] = f"%{brand}%"
+        # if model:
+        #     where_clauses.append("xuan_ze_zi_xi_lie LIKE :model")
+        #     params["model"] = f"%{model}%"
+        # if year:
+        #     where_clauses.append("chu_chang_ri_qi = :year")
+        #     params["year"] = year
+        #
+        # where_sql = " AND ".join(where_clauses) if where_clauses else "1=1"
+        #
+        # table = "accident_car" if car_type == "事故车" else "used_car"
+        # similar_cars = self.db.query(
+        #     f"SELECT site_name, che_liang_pin_pai, xuan_ze_zi_xi_lie, chu_chang_ri_qi, yi_kou_jia FROM {table} WHERE {where_sql} LIMIT 20", params
+        # )
+        #
+        # self.price_table.setRowCount(len(similar_cars))
+        # for i, sc in enumerate(similar_cars):
+        #     self.price_table.setItem(i, 0, QTableWidgetItem(sc.get("site_name", "")))
+        #     self.price_table.setItem(i, 1, QTableWidgetItem(sc.get("che_liang_pin_pai", "")))
+        #     self.price_table.setItem(i, 2, QTableWidgetItem(sc.get("xuan_ze_zi_xi_lie", "")))
+        #     self.price_table.setItem(i, 3, QTableWidgetItem(str(sc.get("chu_chang_ri_qi", ""))))
+        #     self.price_table.setItem(i, 4, QTableWidgetItem(f"¥{sc.get('yi_kou_jia', 0):,.2f}"))
